5c8d18c11e9c663b9671c1a251f4b3c2be6302b0
Anika Abreu <anika.abreu9@gmail.com>
2017-02-07 14:03:44 -0500

    edited images

--- test_portfolio.js
+++ test_portfolio.js
@@ -3,20 +3,20 @@
 $('#linkedin').hover(function() {
     $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/linkedin.jpg")');
 }, function() {
-    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/test_portfolio_BG1.jpg")');
+    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/tracks.jpg")');
 });
 $('#git').hover(function() {
     $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/Bookshelf.jpg")');
 }, function() {
-    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/test_portfolio_BG1.jpg")');
+    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/tracks.jpg")');
 });
 $('#email').hover(function() {
     $('body').css('background-image', 'url("https://0.s3.envato.com/files/90442880/roses8_p.jpg")');
 }, function() {
-    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/test_portfolio_BG1.jpg")');
+    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/tracks.jpg")');
 });
 $('#tel').hover(function() {
     $('body').css('background-image', 'url("https://0.s3.envato.com/files/90442880/roses8_p.jpg")');
 }, function() {
-    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/test_portfolio_BG1.jpg")');
+    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/tracks.jpg")');
 });
