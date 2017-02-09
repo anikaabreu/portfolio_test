54158c24fec135305ef68ce5e3f8feec07949403
Anika Abreu <anika.abreu9@gmail.com>
2017-02-09 14:19:40 -0500

    new files

--- test_portfolio.js
+++ test_portfolio.js
@@ -1,22 +1,32 @@
 // background opacity- can be animated - start and stop duration
+//background opacity
+//move background images to CSS- create a class for each BG image-
+//2. update JQuery to use the animate method on hover that will change the background opacity from 0 to 1
 
-$('#linkedin').hover(function() {
-    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/linkedin.jpg")');
+
+$('#linkedin').hover(function(){
+    $('body').attr('class', 'first_button_bg_hover');
+    $('body').css(['color', 'black']);
 }, function() {
-    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/tracks.jpg")');
-});
+  $('body').attr('class', 'button_bg');
+  });
+
 $('#git').hover(function() {
-    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/Bookshelf.jpg")');
+    $('body').attr('class', 'second_button_bg_hover');
 }, function() {
-    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/tracks.jpg")');
+    $('body').attr('class', 'button_bg');
 });
+
 $('#email').hover(function() {
-    $('body').css('background-image', 'url("https://0.s3.envato.com/files/90442880/roses8_p.jpg")');
+    $('body').attr('class', 'third_button_bg_hover');
+    $('h1').attr('class', 'main_text_hover');
 }, function() {
-    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/tracks.jpg")');
+    $('body').attr('class', 'button_bg');
+    $('h1').attr('class', '#myname');
 });
+
 $('#tel').hover(function() {
-    $('body').css('background-image', 'url("https://0.s3.envato.com/files/90442880/roses8_p.jpg")');
+    $('body').attr('class', 'button_bg');
 }, function() {
-    $('body').css('background-image', 'url("file:///Users/mikosflower/Documents/wyncode/portfolio/test_portfolio/tracks.jpg")');
+    $('body').attr('class', 'button_bg');
 });
